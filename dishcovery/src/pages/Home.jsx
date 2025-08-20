70d394cbe080bca92840812395ce666c4a4c310e
Tambeej <bejella@gmail.com>
2025-08-20 22:52:54 +0300

    you can see the web with Anna's design!!

--- dishcovery/src/pages/Home.jsx
+++ dishcovery/src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Header from "../components/Header";
 
 const Home = () => (
-
+  <div>
+    <Header />
+  </div>
 );
 
 export default Home;
