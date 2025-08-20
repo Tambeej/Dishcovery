0fad78696377246bcf7abff6ef415c4ebab798a8
Tambeej <bejella@gmail.com>
2025-08-21 01:10:30 +0300

    connected recipe page to the api through store

--- dishcovery/src/stores/recipeStore.js
+++ dishcovery/src/stores/recipeStore.js
@@ -1,4 +1,5 @@
 import { makeAutoObservable } from "mobx";
+import { searchRecipes, getRecipeById} from "../services/api";
 
 class RecipeStore {
   searchResults = [];
@@ -10,9 +11,29 @@ class RecipeStore {
     makeAutoObservable(this);
   }
 
-  async fetchRecipes(params) {}
+  async fetchRecipes(params) {
+    this.loading = true;
+    this.error = null;
+    try {
+      this.searchResults = await searchRecipes(params);
+    } catch (err) {
+      this.error = err.message;
+    }
+    this.loading = false;
 
-  async fetchRecipeDetails(id) {}
+  }
+
+  async fetchRecipeDetails(id) {
+    this.loading = true;
+    this.error = null;
+    try {
+      this.currentRecipe = await getRecipeById(id);
+    } catch (err) {
+      this.error = err.message;
+    }
+    this.loading = false;
+  
+  }
 }
 
 const recipeStore = new RecipeStore();
