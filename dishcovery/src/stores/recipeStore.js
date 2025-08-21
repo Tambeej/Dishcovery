9bdfc45e72beb7ab62abc194f8bc69e7211fa542
Tambeej <bejella@gmail.com>
2025-08-21 10:59:41 +0300

    added all functions from api - didnt check if works!

--- dishcovery/src/stores/recipeStore.js
+++ dishcovery/src/stores/recipeStore.js
@@ -63,7 +63,6 @@ class RecipeStore {
       });
     }
   }
-
   //Get all ingredients
   async getAllIngredients() {
     runInAction(() => {
@@ -76,6 +75,85 @@ class RecipeStore {
       runInAction(() => {
         this.ingredientList = ingredientList;
       });
+    } catch (err) {
+      runInAction(() => {
+        this.error = err.message;
+      });
+    } finally {
+      runInAction(() => {
+        this.loading = false;
+      });
+    }
+  }
+
+  // Get recipes by ingredient
+  async fetchRecipesByIngredient(ingredient) {
+    this._withLoading(async () => {
+      const results = await getRecipesByIngredient(ingredient);
+      runInAction(() => {
+        this.searchResults = results;
+      });
+    });
+  }
+
+  // Get random recipe
+  async fetchRandomRecipe() {
+    this._withLoading(async () => {
+      const recipe = await getRandomRecipe();
+      runInAction(() => {
+        this.randomRecipe = recipe;
+      });
+    });
+  }
+
+  // Get categories
+  async fetchCategories() {
+    this._withLoading(async () => {
+      const cats = await getCategories();
+      runInAction(() => {
+        this.categories = cats;
+      });
+    });
+  }
+
+  // Get recipes by category
+  async fetchRecipesByCategory(category) {
+    this._withLoading(async () => {
+      const results = await getRecipesByCategory(category);
+      runInAction(() => {
+        this.searchResults = results;
+      });
+    });
+  }
+
+  // Get areas
+  async fetchAreas() {
+    this._withLoading(async () => {
+      const result = await getAreas();
+      runInAction(() => {
+        this.areas = result;
+      });
+    });
+  }
+
+  // Get recipes by area
+  async fetchRecipesByArea(area) {
+    this._withLoading(async () => {
+      const results = await getRecipesByArea(area);
+      runInAction(() => {
+        this.searchResults = results;
+      });
+    });
+  }
+
+  // Helper to reduce duplication
+  async _withLoading(fn) {
+    runInAction(() => {
+      this.loading = true;
+      this.error = null;
+    });
+    try {
+      await fn();
     } catch (err) {
       runInAction(() => {
         this.error = err.message;
