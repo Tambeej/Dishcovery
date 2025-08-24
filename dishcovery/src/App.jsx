8529791c47ee965639a6c00f1f92201464f6c2c3
Nadavgo8 <19nadav99@gmail.com>
2025-08-24 04:36:56 +0300

    make profile work mobx style (using userstore)

--- dishcovery/src/App.jsx
+++ dishcovery/src/App.jsx
@@ -9,7 +9,6 @@ import Login from "./pages/Login";
 import Search from "./components/Search";
 import Profile from "./pages/Profile";
 import { AuthProvider } from "./auth/AuthProvider";
-
 // import Ingredients from './pages/Ingredients';
 // import Favorites from './pages/Favorites';
 // import Blog from './pages/Blog';
@@ -18,13 +17,21 @@ import Recipe from "./pages/Recipe";
 import RecipesPage from "./pages/RecipesPage";
 import Terms from "./pages/Terms";
 import "./App.css";
+import TopBar from "./components/TopBar";
+import AuthBridge from "./boot/AuthBridge";
 function App() {
   return (
     <AuthProvider>
       <Provider {...rootStore}>
         <Router>
+          <AuthBridge />
           <div className="min-h-screen flex flex-col">
-            {/* <TopBar isLoggedIn={rootStore.user.isLoggedIn} userName={rootStore.user.name} /> */}
+            {
+              <TopBar
+                isLoggedIn={rootStore.userStore.isLoggedIn}
+                userName={rootStore.userStore.name}
+              />
+            }
             {/* <Header /> */}
             <main className="flex-grow">
               <Routes>
@@ -34,12 +41,12 @@ function App() {
 
                 {/* <Route path="/ingredients/:name" element={<Ingredients />} />
                 <Route path="/recipes/ingredient/:ingredient" element={<RecipesPage />} /> */}
-                 <Route path="/terms" element={<Terms />} />
+                <Route path="/terms" element={<Terms />} />
 
                 {/* <Route path="/search" element={<RecipesPage />} />           */}
-               {/* <Route path="/login" element={<Login />} /> */}
-               {/*   <Route path="/profile" element={<Profile />} />             
-                <Route path="/favorites" element={<Favorites />} />
+                {<Route path="/login" element={<Login />} />}
+                <Route path="/profile" element={<Profile />} />
+                {/*<Route path="/favorites" element={<Favorites />} />
                 <Route path="/blog" element={<Blog />} />
                 <Route path="/contact" element={<Contact />} /> */}
               </Routes>
